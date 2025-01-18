93c19948e2a30a3c43bc0484c9670ab54dc64e1a
Apex <robsonlawson3@gmail.com>
2025-01-18 08:18:22 +0000

    Add datagen files
    Add build workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,8 @@ val single = ApexSingleExtension.getOrCreate(project)
 single.withDataGen()
 
 dependencies {
-    implementation("dev.apexstudios:apexcore:21.4.0")
-    accessTransformers("dev.apexstudios:apexcore:21.4.0")
-    interfaceInjectionData("dev.apexstudios:apexcore:21.4.0")
-    "dataImplementation"("dev.apexstudios:apexcore:21.4.0")
+    implementation(libs.apexcore)
+    accessTransformers(libs.apexcore)
+    interfaceInjectionData(libs.apexcore)
+    "dataImplementation"(libs.apexcore)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,6 +19,14 @@ pluginManagement {
     }
 }
 
+dependencyResolutionManagement {
+    versionCatalogs.create("libs") {
+        library("apexcore", "dev.apexstudios", "apexcore").version {
+            strictly("[21.4.0,21.5.0)")
+        }
+    }
+}
+
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
@@ -30,3 +38,5 @@ if(file("../ApexCore/build.gradle.kts").exists()) {
         }
     }
 }
+
+rootProject.name = "ItemResistance"
