382ccdceb49da219cb5f4c9f85c31deaec11293c
Apex <robsonlawson3@gmail.com>
2025-02-15 15:09:12 +0000

    Update to use mod specific github package url

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import dev.apexstudios.gradle.ApexExtension
 import dev.apexstudios.gradle.single.ApexSingleExtension
 import me.modmuss50.mpp.ReleaseType
 
@@ -15,6 +16,10 @@ apex.extendCompilerErrors()
 
 val single = ApexSingleExtension.getOrCreate(project)
 single.withDataGen()
+
+repositories {
+    ApexExtension.withApexStudiosGithubMaven(this, "ApexCore")
+}
 
 dependencies {
     implementation(libs.apexcore)
