87a8de65e907c25b7657a30f99ed25bd6d4c6b11
Apex <robsonlawson3@gmail.com>
2025-02-15 13:52:42 +0000

    Update publishing

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,18 @@ pluginManagement {
         mavenLocal()
 
         maven("https://maven.apexstudios.dev/releases")
+
+        val GITHUB_ACTOR = providers.gradleProperty("gpr.user").orElse(providers.environmentVariable("GITHUB_ACTOR"))
+        val GITHUB_TOKEN = providers.gradleProperty("gpr.token").orElse(providers.environmentVariable("GITHUB_TOKEN"))
+
+        if(GITHUB_ACTOR.isPresent && GITHUB_TOKEN.isPresent) {
+            maven("https://maven.pkg.github.com/ApexStudios-Dev/ApexGradle") {
+                credentials {
+                    username = GITHUB_ACTOR.get()
+                    password = GITHUB_TOKEN.get()
+                }
+            }
+        }
     }
 
     resolutionStrategy {
@@ -28,7 +40,7 @@ dependencyResolutionManagement {
 }
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
 }
 
 if(file("../ApexCore/build.gradle.kts").exists()) {
