7ddcc1ac645fce7d5f940e60a7656118f5c08a1c
Apex <robsonlawson3@gmail.com>
2025-02-25 21:38:48 +0000

    Remove `includeBuild` definitions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,4 @@
 pluginManagement {
-    if(file("../ApexGradle/build.gradle.kts").exists()) {
-        includeBuild("../ApexGradle")
-    }
-
     repositories {
         gradlePluginPortal()
         mavenLocal()
@@ -21,22 +17,12 @@ pluginManagement {
 
 dependencyResolutionManagement {
     versionCatalogs.create("libs") {
-        library("apexcore", "dev.apexstudios", "apexcore").version {
-            strictly("[21.4.0,21.5.0)")
-        }
+        library("apexcore", "dev.apexstudios", "apexcore").version("21.4.19")
     }
 }
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
-}
-
-if(file("../ApexCore/build.gradle.kts").exists()) {
-    includeBuild("../ApexCore") {
-        dependencySubstitution {
-            substitute(module("dev.apexstudios:apexcore")).using(project(":"))
-        }
-    }
 }
 
 rootProject.name = "ItemResistance"
