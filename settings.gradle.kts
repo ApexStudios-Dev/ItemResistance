a510857e5e9ecd0631f95504bda9b1eb0454f1b3
Apex <robsonlawson3@gmail.com>
2025-06-04 20:43:06 +0100

    Bump AC

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,7 @@ pluginManagement {
 
 dependencyResolutionManagement {
     versionCatalogs.create("libs") {
-        library("apexcore", "dev.apexstudios", "apexcore").version("21.5.36-beta-pr-40")
+        library("apexcore", "dev.apexstudios", "apexcore").version("21.5.41-beta-pr-40")
     }
 }
 
