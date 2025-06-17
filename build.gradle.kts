a74a6ff52684e33b38f59ad0e78d39799d51aae4
Apex <robsonlawson3@gmail.com>
2025-06-17 17:34:50 +0100

    Remove private repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,11 +14,6 @@ apex.extendCompilerErrors()
 val single = ApexSingleExtension.getOrCreate(project)
 single.withDataGen()
 
-repositories {
-    maven("https://maven.apexstudios.dev/private")
-    apex.neoPrMaven(this, 2297)
-}
-
 dependencies {
     implementation(libs.apexcore)
     accessTransformers(libs.apexcore)
