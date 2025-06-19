a0ea56640460039a8b705714b6a1afc06a155010
Apex <robsonlawson3@gmail.com>
2025-06-19 01:11:37 +0100

    Update gradle script

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,15 +1,14 @@
 pluginManagement {
     repositories {
         gradlePluginPortal()
-        mavenLocal()
-
         maven("https://maven.apexstudios.dev/releases")
+        maven("https://maven.apexstudios.dev/private")
     }
 
     resolutionStrategy {
         eachPlugin {
             if(requested.id.namespace == "apex-conventions") {
-                useVersion("0.1.58")
+                useVersion("0.1.69")
             }
         }
     }
