9929647f839259e02af9793e311dea9d3d09f61d
Apex <robsonlawson3@gmail.com>
2025-08-15 22:23:22 +0100

    Initial 25w34b port

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 group = "dev.apexstudios"
 
-apex.neoVersion("21.7.11-beta", "1.21.5", "2025.06.15")
+apex.neoVersion("21.9.0-alpha.25w34b.20250821.203312", "1.21.8", "2025.07.20")
 apex.extendCompilerErrors()
 
 val single = ApexSingleExtension.getOrCreate(project)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
 
 dependencyResolutionManagement {
     versionCatalogs.create("libs") {
-        library("apexcore", "dev.apexstudios", "apexcore").version("21.7.0")
+        library("apexcore", "dev.apexstudios", "apexcore").version("21.9.1-beta-pr-49")
     }
 }
 
