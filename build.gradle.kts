a7dc15d02a9e81891c9bb11b62bed8e6bf6b200e
Apex <robsonlawson3@gmail.com>
2025-08-15 22:23:22 +0100

    Initial 21.8 port

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,13 @@
 import dev.apexstudios.gradle.single.ApexSingleExtension
 
 plugins {
-    id("apex-conventions.neoforge") version "0.1.69"
-    id("apex-conventions.immaculate") version "0.1.69"
-    id("apex-conventions.maven-publishing") version "0.1.69"
+    id("apex-conventions.neoforge") version "0.1.71"
+    id("apex-conventions.maven-publishing") version "0.1.71"
 }
 
 group = "dev.apexstudios"
 
-apex.neoVersion("21.7.11-beta", "1.21.5", "2025.06.15")
+apex.neoVersion("21.8.31", "2025.07.20")
 apex.extendCompilerErrors()
 
 val single = ApexSingleExtension.getOrCreate(project)
