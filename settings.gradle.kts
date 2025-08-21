e26991bb529ebe8b9fddd347348de06d89ecda39
Apex <robsonlawson3@gmail.com>
2025-08-21 21:41:50 +0100

    Initial port to 25w34b

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
 
 dependencyResolutionManagement {
     versionCatalogs.create("libs") {
-        library("apexcore", "dev.apexstudios", "apexcore").version("21.8.0-beta-pr-48")
+        library("apexcore", "dev.apexstudios", "apexcore").version("21.9.1-beta-pr-49")
     }
 }
 
