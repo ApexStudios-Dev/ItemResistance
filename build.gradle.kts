c673b84b6675047f56eb6ece8e0e52cb06b329b9
Apex <robsonlawson3@gmail.com>
2025-09-17 16:45:28 +0100

    Update to 21.9-pre1

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,11 +7,15 @@ plugins {
 
 group = "dev.apexstudios"
 
-apex.neoVersion("21.9.0-alpha.25w34b.20250827.140505", "1.21.8", "2025.07.20")
+apex.neoVersion("21.5.0-alpha.1.21.9-pre1.20250917.001820", "1.21.8", "2025.07.20")
 apex.extendCompilerErrors()
 
 val single = ApexSingleExtension.getOrCreate(project)
 single.withDataGen()
+
+repositories {
+    apex.neoPrMaven(this, 2639)
+}
 
 dependencies {
     implementation(libs.apexcore)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
 
 dependencyResolutionManagement {
     versionCatalogs.create("libs") {
-        library("apexcore", "dev.apexstudios", "apexcore").version("21.9.3-beta-pr-51")
+        library("apexcore", "dev.apexstudios", "apexcore").version("21.9.5-beta-pr-51")
     }
 }
 
