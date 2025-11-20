9531842328c3f1921de86859a1008e640274e168
Apex <robsonlawson3@gmail.com>
2025-11-20 14:41:32 +0000

    Swap from JetBrains-Annotations to JSpecify

---
+++ src/data/java/dev/apexstudios/itemresistance/data/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package dev.apexstudios.itemresistance.data;
+
+import org.jspecify.annotations.NullMarked;
---
+++ src/main/java/dev/apexstudios/itemresistance/mixin/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package dev.apexstudios.itemresistance.mixin;
+
+import org.jspecify.annotations.NullMarked;
