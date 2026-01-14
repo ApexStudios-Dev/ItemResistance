e744efb68b2d96ce5cb04501d14f4772ec555096
Apex <robsonlawson3@gmail.com>
2026-01-14 06:24:37 +0000

    Setup version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "dev.apexstudios"
-neoForge.version = "26.1.0.0-alpha.5+snapshot-2"
+neoForge.version = libs.versions.neoforge.get()
 
 repositories {
     maven("https://maven.apexstudios.dev/prs/Registree/pr17") {
@@ -23,12 +23,7 @@ repositories {
 }
 
 dependencies {
-    val registree = "26.1.9-beta-pr-17"
-    implementation("dev.apexstudios:registree:$registree")
-    "dataImplementation"("dev.apexstudios:registree:$registree")
-
-    val apexcore = "26.1.11-beta-pr-70"
-    implementation("dev.apexstudios:apexcore:$apexcore")
-    "dataImplementation"("dev.apexstudios:apexcore:$apexcore")
-    accessTransformers("dev.apexstudios:apexcore:$apexcore")
+    implementation(libs.bundles.apexcore)
+    "dataImplementation"(libs.bundles.apexcore)
+    accessTransformers(libs.apexcore)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,16 @@ pluginManagement {
     }
 }
 
+dependencyResolutionManagement {
+    versionCatalogs.create("libs") {
+        version("neoforge", "26.1.0.0-alpha.5+snapshot-2")
+
+        library("registree", "dev.apexstudios", "registree").version("26.1.9-beta-pr-17")
+        library("apexcore", "dev.apexstudios", "apexcore").version("26.1.11-beta-pr-70")
+        bundle("apexcore", listOf("registree", "apexcore"))
+    }
+}
+
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
