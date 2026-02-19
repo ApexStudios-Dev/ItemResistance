1ebcbad8f7ae4224ad84c2947bad9620f0104391
Apex <robsonlawson3@gmail.com>
2026-02-19 19:34:24 +0000

    Bump to early neo-snap-9 build

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ group = "dev.apexstudios"
 neoForge.version = libs.versions.neoforge.get()
 
 repositories {
-    maven("https://prmaven.neoforged.net/NeoForge/pr2879") {
+    maven("https://prmaven.neoforged.net/NeoForge/pr2975") {
         content {
             includeModule("net.neoforged", "neoforge")
             includeModule("net.neoforged", "testframework")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,10 +15,10 @@ pluginManagement {
 
 dependencyResolutionManagement {
     versionCatalogs.create("libs") {
-        version("neoforge", "26.1.0.0-alpha.11+snapshot-7")
+        version("neoforge", "26.1.0.0-alpha.0+snapshot-9.20260219.013803")
 
-        library("registree", "dev.apexstudios", "registree").version("26.1.16-beta-pr-17")
-        library("apexcore", "dev.apexstudios", "apexcore").version("26.1.26-beta-pr-70")
+        library("registree", "dev.apexstudios", "registree").version("26.1.17-beta-pr-17")
+        library("apexcore", "dev.apexstudios", "apexcore").version("26.1.28-beta-pr-70")
         bundle("apexcore", listOf("registree", "apexcore"))
     }
 }
