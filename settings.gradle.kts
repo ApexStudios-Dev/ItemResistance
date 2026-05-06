f7be96d176f1ed97c1609cb20dd77b4f75fb9b5c
Apex <29412632+ApexModder@users.noreply.github.com>
2026-05-06 23:24:03 +0100

    [26.1] Merge dev into main (#45)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,10 +19,10 @@ pluginManagement {
 
 dependencyResolutionManagement {
     versionCatalogs.create("libs") {
-        version("neoforge", "26.1.0.7-beta")
+        version("neoforge", "26.1.2.42-beta")
 
         library("registree", "dev.apexstudios", "registree").version("26.1.0")
-        library("apexcore", "dev.apexstudios", "apexcore").version("26.1.1")
+        library("apexcore", "dev.apexstudios", "apexcore").version("26.1.2")
         bundle("apexcore", listOf("registree", "apexcore"))
     }
 }
