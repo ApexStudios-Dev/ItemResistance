542c7f64680a77daa0694c84a4326db1a56eb7ba
Apex <robsonlawson3@gmail.com>
2026-05-16 11:49:08 +0100

    Update to support https://github.com/neoforged/NeoForge/pull/3073

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,14 @@ plugins {
 group = "dev.apexstudios"
 neoForge.version = libs.versions.neoforge.get()
 
+repositories {
+    maven("https://prmaven.neoforged.net/NeoForge/pr3073") {
+        content {
+            includeModule("net.neoforged", "neoforge")
+        }
+    }
+}
+
 dependencies {
     implementation(libs.bundles.apexcore)
     "dataImplementation"(libs.bundles.apexcore)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,7 +19,7 @@ pluginManagement {
 
 dependencyResolutionManagement {
     versionCatalogs.create("libs") {
-        version("neoforge", "26.1.2.42-beta")
+        version("neoforge", "26.1.2.109-beta-pr-3073-feat-26.1-new-mods-list-screen")
 
         library("registree", "dev.apexstudios", "registree").version("26.1.0")
         library("apexcore", "dev.apexstudios", "apexcore").version("26.1.2")
