98f0f5bdfbb09ba6a7a444d7430e6b25d666b2d5
Apex <robsonlawson3@gmail.com>
2026-06-06 23:22:37 +0100

    Port to 26.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,27 @@ plugins {
 group = "dev.apexstudios"
 neoForge.version = libs.versions.neoforge.get()
 
+repositories {
+    /*maven("https://prmaven.neoforged.net/NeoForge/pr3198") {
+        content {
+            includeModule("net.neoforged", "neoforge")
+            includeModule("net.neoforged", "testframework")
+        }
+    }*/
+
+    /*maven("https://maven.apexmodder.com/prs/Registree/pr29") {
+        content {
+            includeModule("dev.apexstudios", "registree")
+        }
+    }*/
+
+    /*maven("https://maven.apexmodder.com/prs/ApexCore/pr88") {
+        content {
+            includeModule("dev.apexstudios", "apexcore")
+        }
+    }*/
+}
+
 dependencies {
     implementation(libs.bundles.apexcore)
     "dataImplementation"(libs.bundles.apexcore)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,32 +19,16 @@ pluginManagement {
 
 dependencyResolutionManagement {
     versionCatalogs.create("libs") {
-        version("neoforge", "26.1.2.42-beta")
+        version("neoforge", "26.2.0.0-beta")
 
-        library("registree", "dev.apexstudios", "registree").version("26.1.0")
-        library("apexcore", "dev.apexstudios", "apexcore").version("26.1.2")
+        library("registree", "dev.apexstudios", "registree").version("26.2.0")
+        library("apexcore", "dev.apexstudios", "apexcore").version("26.2.0")
         bundle("apexcore", listOf("registree", "apexcore"))
     }
 }
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
-
-listOf(
-    "Registree",
-    "ApexCore"
-).forEach { lib ->
-    if(file("../../${lib}/26.1").exists()) {
-        includeBuild("../../${lib}/26.1") {
-            name = lib
-
-            dependencySubstitution {
-                substitute(module("dev.apexstudios:${lib.lowercase()}"))
-                    .using(project(":"))
-            }
-        }
-    }
 }
 
 rootProject.name = "ItemResistance"
