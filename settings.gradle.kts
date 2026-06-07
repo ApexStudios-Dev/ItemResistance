02e291f852b5594688783d0f15e7ea26bdf9c052
Apex <robsonlawson3@gmail.com>
2026-06-07 13:46:43 +0100

    Update to pre4

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,10 +19,10 @@ pluginManagement {
 
 dependencyResolutionManagement {
     versionCatalogs.create("libs") {
-        version("neoforge", "26.2.0-alpha.0+pre-2.20260602.164726")
+        version("neoforge", "26.2.0-alpha.0+pre-4.20260607.111935")
 
         library("registree", "dev.apexstudios", "registree").version("26.2.1-beta-pr-29")
-        library("apexcore", "dev.apexstudios", "apexcore").version("26.2.1-beta-pr-88")
+        library("apexcore", "dev.apexstudios", "apexcore").version("26.2.4-beta-pr-88")
         bundle("apexcore", listOf("registree", "apexcore"))
     }
 }
