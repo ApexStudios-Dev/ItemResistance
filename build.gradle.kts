3886d0adcdc7d8cfbba48972c9cf99c503141825
Apex <robsonlawson3@gmail.com>
2026-06-16 17:08:35 +0100

    Bump to 26.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,24 +9,24 @@ group = "dev.apexstudios"
 neoForge.version = libs.versions.neoforge.get()
 
 repositories {
-    maven("https://prmaven.neoforged.net/NeoForge/pr3198") {
+    /*maven("https://prmaven.neoforged.net/NeoForge/pr3198") {
         content {
             includeModule("net.neoforged", "neoforge")
             includeModule("net.neoforged", "testframework")
         }
-    }
+    }*/
 
-    maven("https://maven.apexmodder.com/prs/Registree/pr29") {
+    /*maven("https://maven.apexmodder.com/prs/Registree/pr29") {
         content {
             includeModule("dev.apexstudios", "registree")
         }
-    }
+    }*/
 
-    maven("https://maven.apexmodder.com/prs/ApexCore/pr88") {
+    /*maven("https://maven.apexmodder.com/prs/ApexCore/pr88") {
         content {
             includeModule("dev.apexstudios", "apexcore")
         }
-    }
+    }*/
 }
 
 dependencies {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,10 +19,10 @@ pluginManagement {
 
 dependencyResolutionManagement {
     versionCatalogs.create("libs") {
-        version("neoforge", "26.2.0-alpha.0+pre-4.20260607.111935")
+        version("neoforge", "26.2.0.0-beta")
 
-        library("registree", "dev.apexstudios", "registree").version("26.2.1-beta-pr-29")
-        library("apexcore", "dev.apexstudios", "apexcore").version("26.2.4-beta-pr-88")
+        library("registree", "dev.apexstudios", "registree").version("26.2.0")
+        library("apexcore", "dev.apexstudios", "apexcore").version("26.2.0")
         bundle("apexcore", listOf("registree", "apexcore"))
     }
 }
