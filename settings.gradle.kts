7a678ccbb8c16ecf109ab4522a497c2fdd7a56f9
Apex <robsonlawson3@gmail.com>
2026-07-18 21:21:53 +0100

    Bump versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,8 +21,8 @@ dependencyResolutionManagement {
     versionCatalogs.create("libs") {
         version("neoforge", "26.2.0.1-beta")
 
-        library("registree", "dev.apexstudios", "registree").version("26.2.2-beta-pr-33")
-        library("apexcore", "dev.apexstudios", "apexcore").version("26.2.4-beta-pr-92")
+        library("registree", "dev.apexstudios", "registree").version("26.2.5-beta-pr-33")
+        library("apexcore", "dev.apexstudios", "apexcore").version("26.2.11-beta-pr-92")
         bundle("apexcore", listOf("registree", "apexcore"))
     }
 }
