3854bd1917c3a8d500c18083233342d917736a51
Apex <robsonlawson3@gmail.com>
2026-09-16 00:22:23 +0100

    Add missing pack metadata generator

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,10 +19,10 @@ pluginManagement {
 
 dependencyResolutionManagement {
     versionCatalogs.create("libs") {
-        version("neoforge", "26.3.0.10-beta-pr-3492-pr-data-gen-extensions")
+        version("neoforge", "26.3.0.11-beta-pr-3492-pr-data-gen-extensions")
 
         library("registree", "dev.apexstudios", "registree").version("26.3.2-beta-pr-37")
-        library("apexcore", "dev.apexstudios", "apexcore").version("26.3.4-beta-pr-96")
+        library("apexcore", "dev.apexstudios", "apexcore").version("26.3.5-beta-pr-96")
         bundle("apexcore", listOf("registree", "apexcore"))
     }
 }
--- src/data/java/dev/apexstudios/itemresistance/data/ItemResistanceDataEntryPoint.java
+++ src/data/java/dev/apexstudios/itemresistance/data/ItemResistanceDataEntryPoint.java
@@ -1,6 +1,8 @@
 package dev.apexstudios.itemresistance.data;
 
 import dev.apexstudios.itemresistance.ItemResistance;
+import net.minecraft.data.metadata.PackMetadataGenerator;
+import net.minecraft.network.chat.Component;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
@@ -9,6 +11,9 @@ import net.neoforged.neoforge.data.event.GatherDataEvent;
 @Mod(value = ItemResistance.ID, dist = Dist.CLIENT)
 public final class ItemResistanceDataEntryPoint {
     public ItemResistanceDataEntryPoint(IEventBus modBus) {
-        modBus.addListener(GatherDataEvent.Client.class, event -> event.createProvider(IRLanguageProvider::new));
+        modBus.addListener(GatherDataEvent.Client.class, event -> {
+            event.createProvider(IRLanguageProvider::new);
+            event.createProvider(output -> PackMetadataGenerator.forFeaturePack(output, Component.literal("ItemResistance resources")));
+        });
     }
 }
