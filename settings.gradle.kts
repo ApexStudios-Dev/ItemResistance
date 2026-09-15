5a37c2cfec796f92a65091e5c008628d1d2086f8
Apex <robsonlawson3@gmail.com>
2026-09-15 22:07:53 +0100

    Bump ApexGradle and ApexCore

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,7 @@ pluginManagement {
         resolutionStrategy {
             eachPlugin {
                 if(requested.id.namespace == "apex-conventions") {
-                    useVersion("0.1.101")
+                    useVersion("0.1.102")
                 }
             }
         }
@@ -22,7 +22,7 @@ dependencyResolutionManagement {
         version("neoforge", "26.3.0.0-beta")
 
         library("registree", "dev.apexstudios", "registree").version("26.3.1-beta-pr-37")
-        library("apexcore", "dev.apexstudios", "apexcore").version("26.3.0-beta-pr-96")
+        library("apexcore", "dev.apexstudios", "apexcore").version("26.3.1-beta-pr-96")
         bundle("apexcore", listOf("registree", "apexcore"))
     }
 }
