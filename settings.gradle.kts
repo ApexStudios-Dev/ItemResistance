5fcbc39072d563b03c5256a002035d3cfcb55218
Apex <robsonlawson3@gmail.com>
2026-09-16 00:03:23 +0100

    Bump ApexCore and Registree

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,8 +21,8 @@ dependencyResolutionManagement {
     versionCatalogs.create("libs") {
         version("neoforge", "26.3.0.10-beta-pr-3492-pr-data-gen-extensions")
 
-        library("registree", "dev.apexstudios", "registree").version("26.3.1-beta-pr-37")
-        library("apexcore", "dev.apexstudios", "apexcore").version("26.3.2-beta-pr-96")
+        library("registree", "dev.apexstudios", "registree").version("26.3.2-beta-pr-37")
+        library("apexcore", "dev.apexstudios", "apexcore").version("26.3.4-beta-pr-96")
         bundle("apexcore", listOf("registree", "apexcore"))
     }
 }
